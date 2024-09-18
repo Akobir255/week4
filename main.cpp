42e2dc513f90d64a4f4f4778fe41ee9834785076
Akobir255 <a.toshev@newuu.uz>
2024-09-18 11:25:41 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float x;
+    cin >> x;
+    if (x>-1)
+        cout << x <<" is positive "<< endl;
+    else
+        cout << x <<" is negative "<< endl;
+    return 0;
+}
